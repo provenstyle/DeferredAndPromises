df9a79f779c81512c5051d435529acbcff2cb801
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-11-13 10:28:29 -0600

    Setting up the deferred demo

--- DeferredAndPromises/App/resolvingDeferreds/resolvingDeferreds.js
+++ DeferredAndPromises/App/resolvingDeferreds/resolvingDeferreds.js
@@ -1,34 +1,4 @@
 ﻿define([], function() {
-
-   var def = $.Deferred();
-   var messages = ko.observableArray();
-   
-   var addDone = function() {
-      def.done(function() {
-         messages.push('Done');
-      });
-   };
-
-   var addFail = function () {
-      def.fail(function() {
-         messages.push('Fail');
-
-      });
-   };
-
-   var addAlways = function () {
-      def.always(function () {
-         messages.push('Always');
-      });
-   };
-
-   var resolve = function() {
-      def.resolve();
-   };
-
-   var reject = function() {
-      def.reject();
-   };
    
    return {
       addDone: addDone,
